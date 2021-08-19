249dd9b670db8a858692b6a729c90e828cb57d32
oskhen <oskar.henriksson2001@gmail.com>
2021-08-19 11:09:56 +0200

    Resizes stream to fit screen

--- OpenCV.py
+++ OpenCV.py
@@ -2,23 +2,41 @@ import cv2
 import sys
 import numpy as np
 import subprocess
+import math
 
+marginFactor = 0.9
+
+phoneWidth = 1440
+phoneHeight = 2880
+
+displayHeight = math.floor(1080*marginFactor)
+
+
+displayWidth = math.floor(1440 * (displayHeight / phoneHeight))
+
+print(f"Phone resolution: {phoneWidth}x{phoneHeight}")
+print(f"Converting to display resolution: {displayWidth}x{displayHeight}")
 
 cap = cv2.VideoCapture("/dev/stdin")
 
 if (cap.isOpened()==False):
     print("Failed to open stream")
 
+cv2.namedWindow("main", cv2.WINDOW_AUTOSIZE)
 
 while cap.isOpened():
+
     ret, frame = cap.read()
     if ret == True:
-        cv2.imshow('Frame', frame)
+        frame = cv2.resize(frame, (displayWidth, displayHeight))
+        cv2.imshow('main', frame)
         
         if cv2.waitKey(25) & 0xFF == ord('q'):
             break
     
     else:
         break
+
 cap.release()
 cv2.destroyAllWindows()
+exit()
