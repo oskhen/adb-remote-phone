29074e1652c0911d0c039f3f5c006e0fce543f69
oskhen <oskar.henriksson2001@gmail.com>
2021-08-21 13:41:40 +0200

    Added support for commandline arguments.  Now initalizes with adb devices.

--- OpenCV.py
+++ OpenCV.py
@@ -1,3 +1,4 @@
+from os import path
 import cv2
 import sys
 import numpy as np
@@ -5,78 +6,102 @@ import subprocess
 import math
 import re
 from screeninfo import get_monitors
+import argparse
 
 # TODO: 
-# x/y scaling is off
-# auto-display resolution
-# initADB (is it needed?)
-# adb path
 # swipe
 # fix delays (onClick every time mouse is moved in window!)
 
 # adb shell settings put system show_touches 1
 
 def initADB():
-    pass
+    global path
+    output = str(subprocess.run([path, "devices"], capture_output=True).stdout).split("\\n")
+    devices = [x for x in output if "\\t" in x]
+    if not devices:
+        print("No devices attached. Check your USB connection and try again")
+        exit()
+    elif len(devices) > 1:
+        print("More than one device attached. Disconnect other connected devices and try again")
+        exit()
+    else:
+        device = devices[0].split("\\t")[0]
+        print(f"Device with ID {device} found.")
+
 
 def getResolution():
-    output = str(subprocess.run(["adb", "exec-out", "wm", "size"], capture_output=True).stdout).split("\\n")[0]
+    global path
+    output = str(subprocess.run([path, "exec-out", "wm", "size"], capture_output=True).stdout).split("\\n")[0]
     width, height = map(int, re.sub(r'[^0-9x]', '', output).split("x"))
     return width, height
-    
 
-#- Config
-marginFactor = 0.9
+def initParser():
+    parser = argparse.ArgumentParser(description="Control your Android phone remotely through a USB connection.")
+    parser.add_argument("-adb", "--adb-path", action="store", type=str, dest="PATH", help="Path to ADB. Defaults to adb", default="adb")
+    parser.add_argument("-m", "--margin", action="store", type=int, dest="Margin", help="Height margin in percentage of screen utilised. Defaults to 0.9", default=0.9)
 
-displayHeight = 1080
+    return parser
 
-#|---------
-
-#- Resize calculations
-phoneWidth, phoneHeight = getResolution()
-displayHeight = round(displayHeight*marginFactor)
-scaling = displayHeight / phoneHeight
-displayWidth = round(phoneWidth * scaling)
-#|----------
 
 def onClick(event, x, y, flags, param):
+    global path
 
     if (event == cv2.EVENT_LBUTTONDOWN):
 
         print(f"touch event at {x}, {y}")
-        subprocess.run(["adb", "exec-out", "input", "tap", str(x), str(y)])
+        subprocess.run([path, "exec-out", "input", "tap", str(x), str(y)])
 
 
+def main(config):
+    global path
+
+    #|- Config
+    marginFactor = config.Margin
+    path = config.PATH
+    #|--
+
+    initADB()
+
+    #- Resize calculations
+    phoneWidth, phoneHeight = getResolution()
+    displayHeight = get_monitors()[0].height
+    displayHeight = round(displayHeight*marginFactor)
+    scaling = displayHeight / phoneHeight
+    displayWidth = round(phoneWidth * scaling)
+    #|----------
 
 
+    print(f"Phone resolution: {phoneWidth}x{phoneHeight}")
+    print(f"Converting to display resolution: {displayWidth}x{displayHeight}")
 
+    cap = cv2.VideoCapture("/dev/stdin")
 
+    if (cap.isOpened()==False):
+        print("Failed to open stream")
 
-print(f"Phone resolution: {phoneWidth}x{phoneHeight}")
-print(f"Converting to display resolution: {displayWidth}x{displayHeight}")
+    cv2.namedWindow("main", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("main", displayWidth, displayHeight)
+    cv2.setMouseCallback("main", onClick)
 
-cap = cv2.VideoCapture("/dev/stdin")
+    while cap.isOpened():
 
-if (cap.isOpened()==False):
-    print("Failed to open stream")
+        ret, frame = cap.read()
+        if ret == True:
 
-cv2.namedWindow("main", cv2.WINDOW_NORMAL)
-cv2.resizeWindow("main", displayWidth, displayHeight)
-cv2.setMouseCallback("main", onClick)
-
-while cap.isOpened():
-
-    ret, frame = cap.read()
-    if ret == True:
-
-        cv2.imshow('main', frame)
+            cv2.imshow('main', frame)
+            
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
         
-        if cv2.waitKey(25) & 0xFF == ord('q'):
+        else:
             break
-    
-    else:
-        break
 
-cap.release()
-cv2.destroyAllWindows()
-exit()
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__":
+    parser = initParser()
+    config = parser.parse_args()
+    main(config)
+
